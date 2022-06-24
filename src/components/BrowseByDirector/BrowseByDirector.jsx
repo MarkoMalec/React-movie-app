8e751593dd14582fece5ce332a0369d3b6323b43
Marko <CroPsychooo@yahoo.com>
2022-06-24 16:55:30 +0200

    added browse by director page

--- src/components/BrowseByDirector/BrowseByDirector.jsx
+++ src/components/BrowseByDirector/BrowseByDirector.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE } from '../../fetch';
 import { useParams } from 'react-router-dom';
-import { Container } from '@chakra-ui/react';
+import { Container, Spinner } from '@chakra-ui/react';
 import PersonHeader from '../elements/PersonHeader/PersonHeader';
 import ThumbnailGrid from '../elements/ThumbnailGrid/ThumbnailGrid';
 import Thumbnail from '../elements/Thumbnail/Thumbnail';
@@ -24,7 +24,7 @@ const BrowseByDirector = () => {
     const personResult = await (await fetch(personEndpoint)).json();
     try {
       setDirector(personResult);
-      console.log(personResult);
+    //   console.log(personResult);
     } catch (error) {
       console.error(error);
     }
@@ -38,53 +38,57 @@ const BrowseByDirector = () => {
       );
       setMovies(directedMovies);
       setLoading(false);
-      console.log(directedMovies);
+    //   console.log(directedMovies);
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
+    <>
     <Container pt={120}>
-      <div className="person-page-container">
-        <PersonHeader
-          name={director?.name}
-          birthday={director?.birthday}
-          deathday={director?.deathday}
-          biography={director?.biography}
-          place_of_birth={director?.place_of_birth}
-          gender={director?.gender}
-          profile_path={director?.profile_path}
-          imdb={director?.imdb_id}
-          tmdb={directorId}
-          homepage={director?.homepage}
-        />
-        <ThumbnailGrid
-          preHeader="Movies directed by "
-          header={director?.name}
-          loading={loading}
-        >
-          {movies?.map((el, i) => {
-            return (
-              <Thumbnail
-                key={i}
-                clickable={true}
-                image={
-                  el?.poster_path
-                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${el.poster_path}`
-                    : NoPoster
-                }
-                movieId={el?.id}
-                movieName={el?.title}
-                originalTitle={el?.original_title}
-                releaseDate={el?.release_date}
-                voteAverage={el?.vote_average}
-              />
-            );
-          })}
-        </ThumbnailGrid>
-      </div>
-    </Container>
+    {loading ? <Spinner color='brand.main' size='xl' /> : 
+    <div className="person-page-container">
+      <PersonHeader
+        name={director?.name}
+        birthday={director?.birthday}
+        deathday={director?.deathday}
+        biography={director?.biography}
+        place_of_birth={director?.place_of_birth}
+        gender={director?.gender}
+        profile_path={director?.profile_path}
+        imdb={director?.imdb_id}
+        tmdb={directorId}
+        homepage={director?.homepage}
+      />
+      <ThumbnailGrid
+        preHeader="Movies directed by "
+        header={director?.name}
+        loading={loading}
+      >
+        {movies?.map((el, i) => {
+          return (
+            <Thumbnail
+              key={i}
+              clickable={true}
+              image={
+                el?.poster_path
+                  ? `${IMAGE_BASE_URL}${POSTER_SIZE}${el.poster_path}`
+                  : NoPoster
+              }
+              movieId={el?.id}
+              movieName={el?.title}
+              originalTitle={el?.original_title}
+              releaseDate={el?.release_date}
+              voteAverage={el?.vote_average}
+            />
+          );
+        })}
+      </ThumbnailGrid>
+    </div>
+  }
+  </Container>
+    </>
   );
 };
 
