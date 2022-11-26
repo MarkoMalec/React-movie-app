72785de8b44930ed30ccc64428288b2011a2fddc
Marko Malec <CroPsychooo@yahoo.com>
2022-11-26 18:12:15 +0100

    Added watchlist for TV shows

--- src/components/WatchList/WatchlistComponents/AddToWatchlist.jsx
+++ src/components/WatchList/WatchlistComponents/AddToWatchlist.jsx
@@ -8,21 +8,77 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import { ImEyePlus, ImEyeBlocked, ImCheckmark } from 'react-icons/im';
+import { useLocation } from 'react-router-dom';
 
-const AddToWatchlist = ({ movie }) => {
-  const { addItemToWatchlist, watchlist } = useContext(GlobalContext);
-  const { removeItemFromWatchList } = useContext(GlobalContext);
+const AddToWatchlist = ({ movie, show }) => {
+  const { addMovieToWatchlist, addShowToWatchlist, watchlist, tvWatchlist } =
+    useContext(GlobalContext);
+  const { removeMovieFromWatchlist, removeShowFromWatchlist } =
+    useContext(GlobalContext);
+
+  const location = useLocation();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  let storedMovie = watchlist.find(o => o.id === movie.id);
-  const watchlistDisabled = storedMovie ? true : false;
+  if (movie) {
+    var storedMovie = watchlist.find(o => o.id === movie.id);
+  }
+  if (show) {
+    var storedShow = tvWatchlist.find(o => o.id === show.id);
+  }
+  const watchlistDisabled = storedMovie || storedShow ? true : false;
 
   const modalTimeout = () => {
     setTimeout(() => {
       onClose();
     }, 2000);
   };
+
+  if (
+    location.pathname.includes('TvHome') ||
+    location.pathname.includes('tv')
+  ) {
+    return (
+      <div className="watchlist-thumbnail-wrapper">
+        <ul className="watchlist-controlls">
+          <Modal
+            blockScrollOnMount={false}
+            isOpen={isOpen}
+            onClose={onClose}
+            motionPreset="slideInBottom"
+            size="xs"
+          >
+            <ModalOverlay bg="none" />
+            <ModalContent top="-3.5rem" boxShadow="none">
+              <ModalBody>
+                <p className="watchlist-modal-text-added">
+                  <span>
+                    <ImCheckmark />
+                  </span>
+                  Added to watchlist!
+                </p>
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+          {!watchlistDisabled ? (
+            <li
+              onClick={() => {
+                addShowToWatchlist(show);
+                onOpen();
+                modalTimeout();
+              }}
+            >
+              <ImEyePlus />
+            </li>
+          ) : (
+            <li onClick={() => removeShowFromWatchlist(show.id)}>
+              <ImEyeBlocked />
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
 
   return (
     <div className="watchlist-thumbnail-wrapper">
@@ -49,7 +105,7 @@ const AddToWatchlist = ({ movie }) => {
         {!watchlistDisabled ? (
           <li
             onClick={() => {
-              addItemToWatchlist(movie);
+              addMovieToWatchlist(movie);
               onOpen();
               modalTimeout();
             }}
@@ -57,7 +113,7 @@ const AddToWatchlist = ({ movie }) => {
             <ImEyePlus />
           </li>
         ) : (
-          <li onClick={() => removeItemFromWatchList(movie.id)}>
+          <li onClick={() => removeMovieFromWatchlist(movie.id)}>
             <ImEyeBlocked />
           </li>
         )}
